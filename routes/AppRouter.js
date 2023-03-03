e6d57a18cef0b6b0dc379db35b8be0f2d212bc6d
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-03-03 12:22:41 -0500

    started routes

---
+++ routes/AppRouter.js
@@ -0,0 +1,8 @@
+const Router = require('express').Router()
+const CommentRouter = require('./CommentRouter')
+const UserRouter = require('./UserRouter')
+const ListingRouter = require('./ListingRouter')
+
+Router.use('/users', UserRouter)
+Router.use('/comments', CommentRouter)
+Router.use('/listings', ListingRouter)
