3b7d07c3a1b3b1b65999f6ab7863f900292cd690
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-03-03 17:08:19 -0500

    made some user controllers and routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,56 @@
-const { User } = require("../models")
+const { User, Listing } = require("../models")
 const stringify = require("../utils")
+const middleware = require('../middleware')
 
-const findAllUsers = async (req, res) => {
-    
+const findUserByPk = async (req, res) => {
+    try{
+        const userAndListings = await User.findByPk(req.params.user_id, {
+            include: [{ model: Listing, as: 'listings' }]
+          })
+          stringify(userAndListings)
+          res.send(userAndListings)
+    } catch (error) {
+        throw error
+    }    
+}
+
+const register = async (req, res) => {
+    try{
+        const { email, password, name } = req.body
+        let passwordDigest = await middleware.hashPassword(password)
+        const user = await User.create({ email, passwordDigest, name, rating: null })
+        res.send(user)
+    } catch (error) {
+        throw error
+    }
+}
+
+const signIn = async (req, res) => {
+    try{
+        const user = await User.findOne({
+            where: { email: req.body.email },
+            raw: true
+          })
+          let matched = await middleware.comparePassword(
+            user.passwordDigest, 
+            req.body.password
+          )
+          if(matched){
+            let payload = {
+              id: user.id,
+              email: user.email
+            }
+            let token = middleware.createToken(payload)
+            return res.send({ user: payload, token })
+          }
+          res.status(401).send({ status: 'Error', msg: 'Unauthorized' }) 
+    } catch(error) {
+        throw error
+    }
+}
+
+module.exports = {
+    findUserByPk,
+    register,
+    signIn
 }
