31ef5f8dc367b7c5e3673e02b050cb5eaaea5ba6
Freddy Caione <fac299@nyu.edu>
2023-03-03 12:36:56 -0500

    migrations for user, listing, comment with references + allowNull values

--- migrations/20230303164240-create-user.js
+++ migrations/20230303164240-create-user.js
@@ -1,37 +1,41 @@
-'use strict';
+"use strict"
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      passwordDigest: {
-        type: Sequelize.STRING
-      },
-      rating: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
-  }
-};
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("users", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+        unique: true
+			},
+			passwordDigest: {
+				type: Sequelize.STRING,
+        allowNull: false
+			},
+			rating: {
+				type: Sequelize.INTEGER,
+			},
+			createdAt: {
+        type: Sequelize.DATE,
+				allowNull: false
+			},
+			updatedAt: {
+        type: Sequelize.DATE,
+				allowNull: false
+			},
+		})
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("users")
+	},
+}
--- migrations/20230303164630-create-listing.js
+++ migrations/20230303164630-create-listing.js
@@ -1,37 +1,46 @@
-'use strict';
+"use strict"
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('listings', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.REAL
-      },
-      soldOut: {
-        type: Sequelize.BOOLEAN
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('listings');
-  }
-};
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("listings", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			price: {
+				type: Sequelize.REAL,
+				allowNull: false,
+			},
+			soldOut: {
+				type: Sequelize.BOOLEAN,
+				allowNull: false,
+				defaultValue: false,
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "users",
+					key: "id",
+				},
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		})
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("listings")
+	},
+}
