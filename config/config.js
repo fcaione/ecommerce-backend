8a9e25ef567f605ec54a92b6c2489520773705bd
Freddy Caione <fac299@nyu.edu>
2023-03-09 10:25:41 -0500

    fixing config.js

--- config/config.js
+++ config/config.js
@@ -2,21 +2,20 @@ require("dotenv").config()
 module.exports = {
     development: {
       database: "ecommerce_development",
-      host: "127.0.0.1",
       dialect: "postgres"
     },
     test: {
       database: "ecommerce_test",
-      host: "127.0.0.1",
       dialect: "postgres"
     },
     production: {
       use_env_variable: "DATABASE_URL",
-      host: "127.0.0.1",
       dialect: "postgres",
       dialectOptions: {
-        rejectUnauthorized: false,
-        require: true
+        ssl: {
+          rejectUnauthorized: false,
+          require: true
+        }
       }
     }
 }
